1d6c88d25f96500d34f19273a541a46ffe5c124b
Mariana Silveira <148133489+marisilveirat@users.noreply.github.com>
2023-10-19 19:26:51 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+<meta charset="UTF-8">
+
+<h3>Álcool ou Gasolina?</h3>
+
+<script>
+
+var litros = 40;
+var caminhoComGasolina = 480;
+var usoDeGasolina = caminhoComGasolina/litros;
+var caminhoComAlcool = 300;
+var usoDeAlcool = caminhoComAlcool/litros;
+
+document.write("O consumo de gasolinha é " + usoDeGasolina + " Km/l" + "<br>");
+document.write("O consumo de álcool é " + usoDeAlcool + " Km/l");
+
+</script>
